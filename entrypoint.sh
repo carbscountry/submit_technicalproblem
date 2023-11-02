5c676800ec27f47ef277bb7567fb4be74f286eca
carbscountry <s1270161forbisness@gmail.com>
2023-11-02 17:06:25 +0900

    del endpoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Check if directory exists and create if not
-if [ ! -d "/workspace/output_log" ]; then
-    mkdir -p /workspace/output_log && touch /workspace/output_log/out.log
-fi
-
-# Execute the main command (e.g., start your application)
-exec "$@"
